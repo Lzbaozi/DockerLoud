e143ba1ca5658e3df6f1063ed1c42e5d491dcd88
Lizhe <LZ@LZ-PC>
2017-10-01 23:28:56 +0800

    added config center

--- EmployeeService/src/main/java/com/dockerloud/employeeservice/controller/EmployeeController.java
+++ EmployeeService/src/main/java/com/dockerloud/employeeservice/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.dockerloud.employeeservice.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,9 +19,16 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService service;
+    
+    @Value("${message1}")  
+    private String message1; 
+    
+    @Value("${message2}")  
+    private String message2; 
 
     @RequestMapping(value = "/getEmployee" ,method = RequestMethod.GET)
     public Employee getEmployee(@RequestParam int id) {
+    	System.out.println(message1);
         return service.getEmployee(id);
     }
 
