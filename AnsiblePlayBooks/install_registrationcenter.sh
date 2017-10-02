57d794482a2c7a7cc25eb392ce192d4f5b883619
Lizhe <LZ@LZ-PC>
2017-10-02 18:09:47 +0800

    fix

--- AnsiblePlayBooks/install_registrationcenter.sh
+++ AnsiblePlayBooks/install_registrationcenter.sh
@@ -1,1 +1,1 @@
-ansible -i /root/dockerloud/dockerloud/AnsiblePlayBooks/hosts registrationcenter -a "hostname"
+ansible -i /root/dockerloud/DockerLoud/AnsiblePlayBooks/hosts registrationcenter -a "hostname"
