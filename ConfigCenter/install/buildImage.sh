907b2245a07729309ed32f5c08eab03580740983
Lizhe <LZ@LZ-PC>
2017-10-03 13:36:51 +0800

    added config center install scripts

---
+++ ConfigCenter/install/buildImage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker stop ConfigCenter
+docker rm ConfigCenter
+docker rmi dockerloud/configcenter
+
+docker build -t dockerloud/configcenter .
+
+rm -f /tmp/configcenter.tar
+
+docker save dockerloud/configcenter > /tmp/configcenter.tar
