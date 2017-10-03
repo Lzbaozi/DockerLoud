691b7a91068bb591485024a12e7e84b02823a650
Lizhe <LZ@LZ-PC>
2017-10-03 17:27:50 +0800

    fix

--- Jenkins/deployRegistrationCenter.sh
+++ Jenkins/deployRegistrationCenter.sh
@@ -6,10 +6,12 @@ git pull https://github.com/Lzbaozi/DockerLoud.git
 cd RegistrationCenter
 mvn clean install
 
+cd release_folder
 dos2unix buildImage.sh
 chmod 777 buildImage.sh
 ./buildImage.sh
 
+cd ../../AnsiblePlayBooks/
 dos2unix install_registrationcenter.sh
 chmod 777 install_registrationcenter.sh
 ./install_registrationcenter.sh
