3f5943df400beb677be206773e41ab59fa73734c
Lizhe <LZ@LZ-PC>
2017-10-09 22:43:52 +0800

    fix cookie issue

--- EmployeeLoadBalancerService/src/main/java/com/dockerloud/balancer/employeeservice/controller/EmployeeController.java
+++ EmployeeLoadBalancerService/src/main/java/com/dockerloud/balancer/employeeservice/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package com.dockerloud.balancer.employeeservice.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,8 +40,15 @@ public class EmployeeController {
 	}
 
 	@RequestMapping(value = "/getEmployee", method = RequestMethod.GET)
-	public String getEmployee(@RequestParam int id) {
-		return rt.getForEntity("http://EMPLOYEE-SERVICE/getEmployee?id=" + id, String.class).getBody();
+	public String getEmployee(@RequestParam int id,HttpServletRequest request) {
+		
+		HttpHeaders headers = new HttpHeaders();  
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);  
+		headers.set("Cookie", request.getHeader("Cookie"));
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(null, headers);  
+		ResponseEntity<String> response = rt.exchange( "http://EMPLOYEE-SERVICE/getEmployee?id=" + id, HttpMethod.GET, requestEntity , String.class );  
+		
+		return response.getBody();
 	}
 
 }
